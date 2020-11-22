24fc21f75b8de0b5ccbd8b1c82995f182c6c8546
Kino  Rose <kinoarose@gmail.com>
2020-11-21 20:52:04 -0500

    Stub out data classes

---
+++ src/pm/datas/DatasArmors.hx
@@ -0,0 +1,8 @@
+package pm.datas;
+
+/** @class
+ *   All the armors datas
+ *   @property {SystemArmor[]} list List of all the armors of the game according
+ *   to ID
+ */
+extern class DatasArmors {}
