b3eb279cc87fc1b4a6b221751a832d64fe3c5cb2
Kino  Rose <kinoarose@gmail.com>
2021-07-29 09:00:42 -0400

    Add in the Translatable class

--- src/pm/system/Translatable.hx
+++ src/pm/system/Translatable.hx
@@ -1,6 +1,19 @@
 package pm.system;
 
+import haxe.DynamicAccess;
+
 @:native('Translatable')
-class Translatable extends Base {
-  public var name: Array<String>;
+extern class Translatable extends Base {
+  public function new(?json: DynamicAccess<Dynamic>);
+
+  /**
+   * Assign the default members
+   */
+  override public function setup(): Void;
+
+  public var names: Array<String>;
+
+  public function name(): String;
+
+  public function getCommand(command: Array<String>, iterator: StructIterator): Void;
 }
