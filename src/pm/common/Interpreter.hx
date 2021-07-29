12194b76c8ccd752401c255684c0005b7366ebb2
Kino  Rose <kinoarose@gmail.com>
2021-07-28 21:49:25 -0400

    Add in the interpreter class

---
+++ src/pm/common/Interpreter.hx
@@ -0,0 +1,90 @@
+package pm.common;
+
+import js.lib.Function;
+
+typedef FormulaObjT = {
+  var user: Dynamic;
+  var target: Dynamic;
+  var damage: Float;
+  var thisObject: Dynamic;
+
+  /**
+   * Defaults to true
+   */
+  var ?addReturn: Bool;
+
+  /**
+   * Defaults to ""
+   */
+  var ?additionalName: String;
+
+  /**
+   * Dynamic value
+   * Default to null
+   */
+  var ?additionalValue: Dynamic;
+
+  /**
+   * Defaults to true.
+   */
+  var ?defaultValue: Dynamic;
+}
+
+/**
+ * Interpreter to evaluate formulas or 
+ * simple scripts without having to import.
+ */
+@:native('Interpreter')
+extern class Interpreter {
+  /**
+   * Type of result of the Common class
+   */
+  static var common: String;
+
+  /**
+   * Type of result of the Core class
+   */
+  static var core: String;
+
+  /**
+   * Type of result of the Datas class
+   */
+  static var datas: String;
+
+  /**
+   * Type of EventCommand class.
+   */
+  static var eventCommand: String;
+
+  /**
+   * Type of graphic class.
+   */
+  static var graphic: String;
+
+  /**
+   * Type of the Manager class
+   */
+  static var manager: String;
+
+  /**
+   * Type of the Scene class
+   */
+  static var scene: String;
+
+  /**
+   * Type of the System class
+   */
+  static var system: String;
+
+  /**
+   * Type of the three class/object
+   */
+  static var three: String;
+
+  /**
+   * Type of the howl class/object
+   */
+  static var howl: String;
+
+  public static function evaluate(formula: String, formulaObj: FormulaObjT): Function;
+}
