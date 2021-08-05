e7e903a8e4ebb92195ef40f7d14a6b6d5b8ce887
Kino  Rose <kinoarose@gmail.com>
2021-08-05 07:31:16 -0400

    Add in the events manager class to the list of PaperTea classes

---
+++ src/pm/manager/Events.hx
@@ -0,0 +1,35 @@
+package pm.manager;
+
+import pm.system.MapObject;
+import pm.system.DynamicValue;
+
+@:native('Events')
+@:js.import('../../System/Manager/Events.js')
+extern class Events {
+  function new();
+  static var prototype: Events;
+
+  /**
+    Get the event command and read json.
+  **/
+  static function getEventCommand(json: {}): Dynamic;
+
+  /**
+    Send an event.
+  **/
+  static function sendEvent(sender: MapObject, targetKind: Float, targetID: Float, isSystem: Bool, eventID: Float,
+    parameters: Array<DynamicValue>, senderNoReceiver: Bool, onlyTheClosest: Bool): Void;
+
+  /**
+    Send an event detection
+  **/
+  static function sendEventDetection(sender: MapObject, targetID: Float, isSystem: Bool, eventID: Float,
+    parameters: Array<Dynamic>, ?senderNoReceiver: Bool, ?onlyTheClosest: Bool): Void;
+
+  /**
+    Send an event to objects.
+  **/
+  static function sendEventObjects(objects: Array<MapObject>, sender: MapObject, targetID: Float, isSystem: Bool,
+    eventID: Float, parameters: Array<Dynamic>, senderNoReceiver: Bool, onlyTheClosest: Bool,
+    closests: Array<Array<Dynamic>>): Void;
+}
