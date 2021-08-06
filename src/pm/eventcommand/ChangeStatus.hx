17dfbac97c13c88ade221f0b7a8eaf685eb3c7f5
Kino  Rose <kinoarose@gmail.com>
2021-08-05 21:39:59 -0400

    Add in the change status event command as well

---
+++ src/pm/eventcommand/ChangeStatus.hx
@@ -0,0 +1,22 @@
+package pm.eventcommand;
+
+import pm.system.MapObject;
+import pm.system.DynamicValue;
+
+@:native('ChangeStatus')
+@:js.import('../../System/EventCommand/ChangeStatus.js')
+extern class ChangeStatus extends Base {
+  function new(command: Array<Dynamic>);
+  var selection: Float;
+  var heInstanceID: DynamicValue;
+  var groupIndex: Float;
+  var operation: Float;
+  var statusID: DynamicValue;
+
+  /**
+    Update and check if the event is finished.
+  **/
+  function update(currentState: {}, object: MapObject, state: Float): Float;
+
+  static var prototype: ChangeStatus;
+}
