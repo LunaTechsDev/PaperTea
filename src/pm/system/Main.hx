1bd52e155dd59a31f155b19a9ae46deed38e0a52
inc0der <inc0der.code@gmail.com>
2021-08-17 19:11:30 -0300

    Add loaded var and fix js import path

--- src/pm/system/Main.hx
+++ src/pm/system/Main.hx
@@ -4,8 +4,9 @@ package pm.system;
  *  The main class which sets up data and begins the main game loop.
  */
 @:native('Main') ;
-@:js.import('../../System/System/Main.js') ;
+@:js.import('../../System/Main.js') ;
 extern class Main {
+  public static loaded: Bool;
   function new();
   public static function initialize(): Void;
   public static function load(): Void;
