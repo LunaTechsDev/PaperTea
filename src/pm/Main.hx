f01aab87ef7b6b40156bd850a388a553fd1cafb3
inc0der <inc0der.code@gmail.com>
2021-08-18 12:38:09 -0300

    Fix js import path

--- src/pm/Main.hx
+++ src/pm/Main.hx
@@ -3,7 +3,7 @@ package pm;
 /** @class
  *  The main class which sets up data and begins the main game loop.
  */
-@:js.import('../../Main.js')
+@:js.import('../../System/main.js')
 extern class Main {
   public static var loaded: Bool;
   public function new(): Void;
