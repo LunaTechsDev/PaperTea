d6a093216751729cee0b42592a7aaca8f7296cfd
inc0der <inc0der.code@gmail.com>
2021-08-20 01:25:03 -0300

    Add a CI enviroment conditional

--- src/core/Macros.hx
+++ src/core/Macros.hx
@@ -11,6 +11,7 @@ import haxe.Json;
 class Macros {
   #if macro
   static var pluginDir = '${Sys.getCwd()}/game/resources/app/Content/Datas/Scripts/Plugins';
+  static var CI_ENV: Bool = Sys.getEnv('CI') != null;
 
   public static function getPluginName() {
     var data = File.getContent('${Sys.getCwd()}/details.json');
@@ -38,7 +39,7 @@ class Macros {
     if (isDist != null) {
       Compiler.setOutput('${Sys.getCwd()}/dist/code.js');
       File.copy('${Sys.getCwd()}/details.json', '${Sys.getCwd()}/dist/details.json');
-    } else {
+    } else if (!CI_ENV) {
       Compiler.setOutput('${Macros.pluginDir}/${pluginName}/code.js');
     }
   }
@@ -48,8 +49,12 @@ class Macros {
       var pluginName = getPluginName();
       var pluginDir = '${Macros.pluginDir}/${pluginName}/';
       var distDir = '${Sys.getCwd()}/dist/';
-      Sys.command('npx napkin --path=${pluginDir}');
-      Sys.command('npx napkin --path=${distDir}');
+      if (CI_ENV) {
+        Sys.command('npx napkin --path=${distDir}');
+      } else {
+        Sys.command('npx napkin --path=${pluginDir}');
+        Sys.command('npx napkin --path=${distDir}');
+      }
     });
   }
   #end
